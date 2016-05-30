4787d481f7214c0283e90973dfcf7c4f20d48eb8
xu xiaoyan <xu xiaoyan>
2016-05-30 09:54:59 +0800

    uuuuu

--- js/index.js
+++ js/index.js
@@ -8,20 +8,15 @@ window.onload = function(){
 	var oBack4 = document.getElementById("back-4");
 	var oBack5 = document.getElementById("back-5");
 	var oBack6 = document.getElementById("back-6");
-
-		var json = {webkit:"webkitTransform",moz:"mozTransform",o:"oTransform"}
+		var json = {webkit:"webkitTransform",moz:"MozTransform",o:"OTransform",ms:"msTransform"}
 		function addPrev(obj,tx,ty,rx,ry){
-			
 			for(var name in json){
-				obj.style[json[name]] = "translateX("+tx+"px) translateY("+ty+"px) rotateX("+rx+"deg) rotateY("+ry+"deg)";
 				
+				obj.style[json[name]] = "translateX("+tx+"px) translateY("+ty+"px) rotateX("+rx+"deg) rotateY("+ry+"deg)";
 			}
-			console.log(json[name]);
-			
-
 		}
-		//addPrev(oBack1,0.01,0.01,0.001,0.001);
-		
+
+		addPrev(oBack1,0.01,0.01,0.001,0.001);
 	oMain.onmousemove = function(ev){
 		var oEvent = ev || event;
 		
@@ -33,13 +28,7 @@ window.onload = function(){
 		addPrev(oBack4,scalX*0.2,scalY*0.1,scalX*0.003,scalY*0.003);
 		addPrev(oBack5,scalX*0.05,scalY*0.01,scalX*0.001,scalY*0.001);
 		addPrev(oBack6,scalX*0.25,scalY*0.15,scalX*0.01,scalY*0.01);
-		//console.log(scalY,scalX)
-		/*oBack1.style.webkitTransform  = "translateX("+scalX*0.01+"px) translateY("+scalY*0.01+"px) rotateX("+scalX*0.001+"deg) rotateY("+scalY*0.001+"deg)" ;
-		oBack2.style.webkitTransform  = "translateX("+scalX*0.01+"px) translateY("+scalY*0.01+"px) rotateX("+scalX*0.001+"deg) rotateY("+scalY*0.001+"deg)" ;
-		oBack3.style.webkitTransform  = "translateX("+scalX*0.01+"px) translateY("+scalY*0.01+"px) rotateX("+scalX*0.001+"deg) rotateY("+scalY*0.001+"deg)" ;
-		oBack4.style.webkitTransform  = "translateX("+scalX*0.2+"px) translateY("+scalY*0.1+"px) rotateX("+scalX*0.03+"deg) rotateY("+scalY*0.03+"deg)" ;
-		oBack5.style.webkitTransform  = "translateX("+scalX*0.05+"px) translateY("+scalY*0.01+"px) rotateX("+scalX*0.001+"deg) rotateY("+scalY*0.001+"deg)" ;
-		oBack6.style.webkitTransform  = "translateX("+scalX*0.25+"px) translateY("+scalY*0.15+"px) rotateX("+scalX*0.01+"deg) rotateY("+scalY*0.01+"deg)" ;*/
+		
 	
 	}
 }
